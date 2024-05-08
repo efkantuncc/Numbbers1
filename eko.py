b331f7d404242fde57dbc89b1bce479a85409636
efkantuncc <125455467+efkantuncc@users.noreply.github.com>
2024-05-08 20:08:52 +0300

    Update eko.py

--- eko.py
+++ eko.py
@@ -1,34 +1,29 @@
 import telebot
 from pytube import YouTube
-from tiktokapi import TikTokApi
 import instaloader
 
 # Telegram botunuzun token'ını buraya ekleyin
 TOKEN = '7005558840:AAFFQZ0V5rkSYbuqoG4adafJAS0liUrKnw0'
 
 bot = telebot.TeleBot(TOKEN)
-tiktok_api = TikTokApi()
 insta_loader = instaloader.Instaloader()
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "💫 Merhaba! Ben Eko Download, Instagram, YouTube, TikTok platformlarından video indirme yardımcı olmak için üretildim.\nAşağıdaki butonları kullanarak istediğiniz platformdan video indirebilirsiniz.")
+    bot.reply_to(message, "💫 Merhaba! Ben Eko Download, Instagram ve YouTube platformlarından video indirme yardımcı olmak için üretildim.\nAşağıdaki butonları kullanarak istediğiniz platformdan video indirebilirsiniz.")
 
     # Butonları oluştur
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    tiktok_button = types.KeyboardButton("📙 TikTok")
-    instagram_button = types.KeyboardButton("📗 Instagram")
-    youtube_button = types.KeyboardButton("📕 YouTube")
-    owner_button = types.KeyboardButton("⚜️ Sahip")
-    keyboard.add(tiktok_button, instagram_button, youtube_button, owner_button)
+    keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    instagram_button = telebot.types.KeyboardButton("📗 Instagram")
+    youtube_button = telebot.types.KeyboardButton("📕 YouTube")
+    owner_button = telebot.types.KeyboardButton("⚜️ Sahip")
+    keyboard.add(instagram_button, youtube_button, owner_button)
 
     bot.send_message(message.chat.id, "Hangi platformdan video indirmek istersiniz?", reply_markup=keyboard)
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    if message.text == "📙 TikTok":
-        bot.reply_to(message, "TikTok platformundan video indirmek için lütfen TikTok video linkini gönderin.")
-    elif message.text == "📗 Instagram":
+    if message.text == "📗 Instagram":
         bot.reply_to(message, "Instagram platformundan video indirmek için lütfen Instagram video linkini gönderin.")
     elif message.text == "📕 YouTube":
         bot.reply_to(message, "YouTube platformundan video indirmek için lütfen YouTube video linkini gönderin.")
@@ -38,13 +33,7 @@ def handle_message(message):
 @bot.message_handler(content_types=['text'])
 def download_video(message):
     try:
-        if message.text.startswith("https://www.tiktok.com/") or message.text.startswith("https://vm.tiktok.com/"):
-            video_id = message.text.split('/')[-1]
-            tiktok_video = tiktok_api.get_video_by_id(video_id)
-            with open(f"{video_id}.mp4", "wb") as out:
-                out.write(tiktok_video)
-            bot.reply_to(message, "TikTok video başarıyla indirildi!")
-        elif "instagram.com" in message.text:
+        if "instagram.com" in message.text:
             insta_loader.download_post(message.text, target='.')
             bot.reply_to(message, "Instagram video başarıyla indirildi!")
         elif "youtube.com" in message.text:
